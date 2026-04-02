1630a8bd4806a8b70c44b21c00628f3f04898bb9
Samrat Ghimire <samratghimire32@gmail.com>
2026-04-02 23:05:58 +0545

    ignore ts errors on build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Allow production builds to succeed even when TypeScript reports errors.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
